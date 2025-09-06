bf9b924805267101e83546d97822f8d493f815b4
manikishore-jaddu <mani.kishore307@gmail.com>
2025-09-06 08:20:54 +0530

    06-09-2025 variables and datatypes

---
+++ src/practice/day1.js
@@ -0,0 +1,27 @@
+// Declare your name, age, and whether you are learning JS (true/false).
+let name = 'mani';
+let age = 32;
+let isLeraningJs = true;
+
+// Print them in a sentence like:
+// 👉 "My name is Mani, I am 25 years old, learning JS: true"
+
+console.log(`My name is ${name}, I am ${age} years old, learning Js : ${isLeraningJs}`);
+
+
+// Create a variable with null and another with undefined, print them.
+let firstName = null;
+let marks;
+console.log(firstName);
+console.log(marks);
+
+
+// Make an object book with properties: title, author, year, and print them.
+
+const book = {
+    title: 'Java-script-basics',
+    auther: 'mani',
+    year: 2025
+}
+
+console.log(`book name ${book.title} written by ${book.auther} printed in : ${book.year}`);
